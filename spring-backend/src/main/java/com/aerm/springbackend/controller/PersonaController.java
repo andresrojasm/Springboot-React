e6fb74afc5e2e75760eb7685cf0ca6c7fda19cb6
Andres Rojas <anderojasm@ulatina.net>
2023-12-10 23:12:55 -0600

    Adding CORS to Persona Controller

--- spring-backend/src/main/java/com/aerm/springbackend/controller/PersonaController.java
+++ spring-backend/src/main/java/com/aerm/springbackend/controller/PersonaController.java
@@ -3,6 +3,7 @@ package com.aerm.springbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.aerm.springbackend.model.Persona;
 import com.aerm.springbackend.repository.PersonaRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class PersonaController {
